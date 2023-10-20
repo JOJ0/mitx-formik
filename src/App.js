26789b10ed63f97a46c5abd5a235f5c112ae77c0
J0J0 Todos <jojo@peek-a-boo.at>
2023-10-20 11:46:53 +0200

    Add form (email,pw) and some validation
    - Email and Password fields checked for being filled at all.
    - "touched handling" (not sure if that is correctly working already).
    - Email format is regex-checked.
    - Field names and error messages according to requirements.
    - Style for errors in App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-// TODO: import useFormik from formik library
+import {useFormik} from 'formik'
 
 function App() {
-  // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    initialTouched: {
+      email: false,
+      password: false
+    },
+    onSubmit: values => {
+      console.log('form:', values);
+    },
+    validate: values => {
+      let errors = {};
+      if (!values.password) errors.password = 'Field required';
+      if (!values.email) {
+        errors.email = 'Field required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Username should be an email address.';
+      }
+      return errors;
+    }
+  });
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Username</div>
+        <input name="emailField" type="text" onChange={formik.handleChange} value={formik.values.emailField} onBlur={formik.handleBlur}/>
+        {formik.errors.email && formik.touched.email ? <div name="emailError" className="formikError">{formik.errors.email}</div> : null}
+        <div>Password</div>
+        <input name="pswField" type="text" onChange={formik.handleChange} value={formik.values.password} onBlur={formik.handleBlur}/>
+        {formik.errors.password && formik.touched.password ? <div name="pswError" className="formikError">{formik.errors.password}</div> : null}
+        <button name="submitBtn" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
